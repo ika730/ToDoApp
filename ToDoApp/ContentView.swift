0beb97e2ae351ead7c5821d048da6650e0a1cbe1
Nakagawa Kosuke <kosukenakagawa730@gmail.com>
2023-08-18 04:50:54 +0900

    secondCommit

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
+struct TaskData: Identifiable{
+    var id = UUID()
+    var title: String
+    var completed: Bool
+    
+}
+
 struct ContentView: View {
+    @State var taskData = [
+        TaskData(title: "ジョギングする", completed: false),
+        TaskData(title: "お花に水をやる", completed: false),
+        TaskData(title: "部屋の掃除をする", completed: false),
+        TaskData(title: "本を読む", completed: false)
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            List(0..<taskData.count, id:\.self) { index in
+                Button{
+                    taskData[index].completed = !taskData[index].completed
+                    
+                }label:{
+                    HStack {
+                        Image(systemName: taskData[index].completed ? "checkmark.circle.fill" : "circle")
+                        Text(taskData[index].title)
+                    }
+                }
+                .foregroundColor(.primary)
+            }
+            .navigationTitle("ToDoリスト")
         }
-        .padding()
+        
     }
 }
 
