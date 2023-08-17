18bec67f72a1422ff9436483b52cb5b7d3a3be9a
Nakagawa Kosuke <kosukenakagawa730@gmail.com>
2023-08-18 04:05:46 +0900

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Nakagawa Kosuke on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
